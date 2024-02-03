593e28e4b5fe334f7529196e2c94bacad631e7cc
Brandon Bruce <brandon@DESKTOP-6M2VKBU>
2024-02-03 12:56:16 -0500

    step 1

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -11,7 +11,26 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it('should not add new server on submitServerInfo() if name input is an empty string', function () {
+    serverNameInput.value = '';
+    submitServerInfo();
+
+    expect(Object.keys(allServers).length).toEqual(0);
+  })
+
+  it('should generate no empty table rows on updateServeTable()', function () {
+    submitServerInfo();
+
+    let tds = document.querySelectorAll('#serverTable tbody tr td');
+    expect(tds.length).toEqual(2);
+    expect(tds[0].innerText).not.toEqual('');
+    expect(tds[1].innerText).not.toEqual('');
+  })
+
   afterEach(function() {
     // teardown logic
+    allServers = {};
+    serverId = 0;
+    serverTbody.innerHTML = '';
   });
 });
